01c04320b54e64ab2c0c52f165a345dd5ccaf9ee
Lim Lei <87996616+limlei2@users.noreply.github.com>
2025-03-07 23:26:47 +0000

    backend

---
+++ backend/src/main/java/dev/limlei2/emailWriterAI/EmailWriterAiApplication.java
@@ -0,0 +1,13 @@
+package dev.limlei2.emailWriterAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterAiApplication.class, args);
+	}
+
+}
