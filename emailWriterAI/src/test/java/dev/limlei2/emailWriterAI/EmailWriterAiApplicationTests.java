e44c059f2cf1d9bdbf400a6851f532e8a1269897
Lim Lei <87996616+limlei2@users.noreply.github.com>
2025-03-07 23:23:15 +0000

    backend files

---
+++ emailWriterAI/src/test/java/dev/limlei2/emailWriterAI/EmailWriterAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limlei2.emailWriterAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
