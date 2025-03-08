c2f2113bb480bc4d67fbecfc6bffd06a443fefd9
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-08 02:44:26 -0500

    init recipe page and navbar and fixed email \n

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { EmailPageComponent } from './components/email-page/email-page.component';
+import { RecipePageComponent } from './components/recipe-page/recipe-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'email',
         component:EmailPageComponent
+    },
+    {
+        path:'recipe',
+        component:RecipePageComponent
     }
 ];
---
+++ frontend/src/app/components/recipe-page/recipe-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-page',
+  imports: [],
+  templateUrl: './recipe-page.component.html',
+  styleUrl: './recipe-page.component.css'
+})
+export class RecipePageComponent {
+
+}
