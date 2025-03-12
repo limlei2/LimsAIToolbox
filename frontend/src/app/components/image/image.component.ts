37e4e73d92d5ac6ccb4fefd17fcf6fa030ed0efa
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-12 19:44:50 -0400

    image frontend, cors and prompt not working

--- frontend/src/app/components/image/image.component.ts
+++ frontend/src/app/components/image/image.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AutosizeModule } from 'ngx-autosize';
 
 @Component({
   selector: 'app-image',
-  imports: [],
+  imports: [FormsModule, AutosizeModule],
   templateUrl: './image.component.html',
   styleUrl: './image.component.css'
 })
 export class ImageComponent {
+
+  prompt: String = '';
+  imgUrls: any[] = [];
+
+  http = inject(HttpClient);
+  apiUrl = 'http://localhost:8080/api/image'
+
+  onSubmit(){
+    console.log(prompt);
+    console.log(`${this.apiUrl}/generate?prompt=${prompt}`);
+    this.http.get(`${this.apiUrl}/generate?prompt=${prompt}`).subscribe((result: any) => {
+      console.log(result);
+      this.imgUrls = result;
+      console.log(this.imgUrls);
+    })
+  }
 
 }
