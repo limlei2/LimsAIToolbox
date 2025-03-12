0bbd318711de76663ffbab45bcceba984b4621c9
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-11 20:55:07 -0400

    audio backend

---
+++ backend/src/main/java/dev/limlei2/limsToolbox/audio/AudioController.java
@@ -0,0 +1,52 @@
+package dev.limlei2.limsToolbox.audio;
+
+import lombok.AllArgsConstructor;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/audio")
+@CrossOrigin(origins = "*")
+public class AudioController {
+
+    private final OpenAiAudioTranscriptionModel transcriptionModel;
+
+    public AudioController(@Value("${spring.ai.openai.api-key}") String apiKey){
+        OpenAiAudioApi openAiAudioApi = new OpenAiAudioApi(apiKey);
+        this.transcriptionModel
+                = new OpenAiAudioTranscriptionModel(openAiAudioApi);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> transcribeAudio(@RequestParam("file") MultipartFile file) throws IOException {
+        File tempFile = File.createTempFile("audio", ".wav");
+        file.transferTo(tempFile);
+
+        OpenAiAudioTranscriptionOptions transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .language("en")
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .temperature(0f)
+                .build();
+
+        FileSystemResource audioFile = new FileSystemResource(tempFile);
+
+        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audioFile, transcriptionOptions);
+        AudioTranscriptionResponse response = transcriptionModel.call(transcriptionRequest);
+        tempFile.delete();
+        return new ResponseEntity<>(response.getResult().getOutput(), HttpStatus.OK);
+    }
+
+
+}
