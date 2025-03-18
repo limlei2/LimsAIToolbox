49c9fce5948cfe42ecd4e37c9e599a98f9b38512
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-18 17:11:05 -0400

    buildspec.yml

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://aitoolbox-backend.us-east-2.elasticbeanstalk.com'
+    // apiUrl: 'https://toolbox-deployment-latest.onrender.com'
+    //apiUrl: 'http://localhost:8080'
   };
