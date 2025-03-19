96e8efd14c6b5f46588c54f32d0f2bb05cbc403d
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-18 22:40:23 -0400

    update

--- frontend/src/app/components/email-page/email-page.component.ts
+++ frontend/src/app/components/email-page/email-page.component.ts
@@ -26,6 +26,7 @@ export class EmailPageComponent {
   http = inject(HttpClient);
   
   onSubmit(){
+    console.log(`${this.apiUrl}/generate`);
     this.response = '';
     this.loading = true;
     this.http.post(`${this.apiUrl}/generate`, this.emailObj, {responseType: 'text'}).subscribe((result: any) => {
