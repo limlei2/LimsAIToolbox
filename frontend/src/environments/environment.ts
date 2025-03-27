63aea7eb056be6118419775f1c2d68eef01e17b8
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-26 21:05:17 -0400

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
     production: false,
     apiUrl: import.meta.env['NG_APP_BACKEND_URL'] as string
-    // apiUrl: 'https://toolbox-deployment-latest.onrender.com'
-    // apiUrl: 'http://localhost:8080'
-    // http://aitoolbox-backend.us-east-2.elasticbeanstalk.com
   };
